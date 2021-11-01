a3692ee213f056f1ad32442c4be0b863621c417a
PrincessAdora <89851664+PrincessAdora@users.noreply.github.com>
2021-11-01 12:32:00 -0400

    PropsPractice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import Navbar from './components/Navbar.js';
+import React from "react";
+import Navbar from "./components/Navbar.js";
 // Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
-import './App.css';
+import SchoolCard from "./components/SchoolCard.js";
+import data from "./nycSchoolEnrollmentData.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="container">
         <div className="row">
-
+          <SchoolCard
+            school={data[0]}
+            perFem={data[0]}
+            perMa={data[0]}
+            perBlack={data[0]}
+          />
+          <SchoolCard
+            school={data[1]}
+            perFem={data[1]}
+            perMa={data[1]}
+            perBlack={data[1]}
+          />
+          <SchoolCard
+            school={data[2]}
+            perFem={data[2]}
+            perMa={data[2]}
+            perBlack={data[2]}
+          />
+          <SchoolCard
+            school={data[3]}
+            perFem={data[3]}
+            perMa={data[3]}
+            perBlack={data[3]}
+          />
+          <SchoolCard
+            school={data[4]}
+            perFem={data[4]}
+            perMa={data[4]}
+            perBlack={data[4]}
+          />
+          <SchoolCard
+            school={data[5]}
+            perFem={data[5]}
+            perMa={data[5]}
+            perBlack={data[5]}
+          />
+          <SchoolCard
+            school={data[6]}
+            perFem={data[6]}
+            perMa={data[6]}
+            perBlack={data[6]}
+          />
+          <SchoolCard
+            school={data[7]}
+            perFem={data[7]}
+            perMa={data[7]}
+            perBlack={data[7]}
+          />
+          <SchoolCard
+            school={data[8]}
+            perFem={data[8]}
+            perMa={data[8]}
+            perBlack={data[8]}
+          />
+          <SchoolCard
+            school={data[9]}
+            perFem={data[9]}
+            perMa={data[9]}
+            perBlack={data[9]}
+          />
         </div>
       </div>
     </div>
--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './css/SchoolCard.css';
+import React from "react";
+import { isPropertySignature } from "typescript";
+import "./css/SchoolCard.css";
 
-function SchoolCard() {
-  return(
+function SchoolCard(props) {
+  return (
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.school.ENTITY_NAME}</h5>
         <div className="card-body">
           <h5 className="card-title">
             <span className="badge">2017-18</span>
@@ -14,9 +15,15 @@ function SchoolCard() {
           </h5>
           <span className="badge badge-header">Student Data</span>
           <ul className="list-group">
-            <li className="list-group-item">Enrollment Data</li>
-            <li className="list-group-item">3-8 ELA Assessment Data</li>
-            <li className="list-group-item">3-8 Math Assessment Data</li>
+            <li className="list-group-item">
+              Percent Female: {props.perFem.PER_FEMALE}
+            </li>
+            <li className="list-group-item">
+              Percent Male: {props.perMa.PER_MALE}
+            </li>
+            <li className="list-group-item">
+              Percent Black: {props.perBlack.PER_BLACK}
+            </li>
           </ul>
           <span className="badge badge-header">School Data</span>
           <ul className="list-group">
